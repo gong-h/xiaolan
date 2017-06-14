20a41d7af00f3905d56e8fda239a0f5c8ba2ab64
fiendo <fiendo@foxmail.com>
2017-06-14 20:44:18 +0800

    1.integrate the spring-mvc spring-web.2.create LoginFilter

---
+++ src/main/java/com/xiaolan/common/filter/LoginFilter.java
@@ -0,0 +1,9 @@
+package com.xiaolan.common.filter;
+
+/**
+ * @author fiendo
+ * @version 1.0 (2017/6/14)
+ * @description:
+ */
+public class LoginFilter {
+}
---
+++ src/test/java/com/TestController.java
@@ -0,0 +1,9 @@
+package com;
+
+/**
+ * @author fiendo
+ * @version 1.0 (2017/6/14)
+ * @description:
+ */
+public class TestController {
+}
